ca1de2e4d6bce81c3589b8bd05b2f2683cc00231
Lee-Seonghyun316 <70502670+Lee-Seonghyun316@users.noreply.github.com>
2022-01-19 13:02:58 +0900

    [ADD] publish slide Des

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,11 +1,23 @@
 import styled from "styled-components";
 import React from "react";
 
-const Item = ({ src, active, prev, next }) => {
-  return <ItemStyle src={src} active={active} prev={prev} next={next} alt="" />;
+const Item = ({src, active, prev, next}) => {
+    return (
+        <Wrap active={active} prev={prev} next={next}>
+            <ImgContainer>
+                <ItemStyle src={src} alt=""/>
+                <Des>
+                    <DesTitle>
+                        포트폴리오를 부탁해!
+                    </DesTitle>
+                    <DesText>디자이너의 포폴 살펴보기</DesText>
+                    <Move>바로가기 ></Move>
+                </Des>
+            </ImgContainer>
+        </Wrap>);
 };
 
-const ItemStyle = styled.img`
+const Wrap = styled.div`
   opacity: 0;
   position: absolute;
   top: 0;
@@ -19,13 +31,58 @@ const ItemStyle = styled.img`
   transform: ${(props) => (props.next ? "translateX(100%)" : "")};
   z-index: ${(props) => (props.prev || props.next ? 800 : "")};
 
-  ${({ active }) =>
+  ${({active}) =>
     active &&
     `
     opacity: 1;
     position: relative;
     z-index: 900;
-  `}
+  `}`;
+
+const ImgContainer = styled.div`
+position: relative;
+`;
+
+const Des = styled.div`
+position: absolute;
+background: white;
+bottom: 28px;
+left: 7%;
+width: 330px;
+border-radius: 5px;
+text-align: left;
+opacity: 0;
+@media (min-width: 1200px){
+opacity: 1;
+}
+`;
+
+const DesTitle = styled.h2`
+margin: 20px 20px 0 20px;
+font-size: 20px;
+line-height: 1.5;
+`
+
+const DesText = styled.h3`
+font-weight: 500;
+margin: 0 20px;
+font-size: 14px;
+height: 44px;
+line-height: 1.64;
+`;
+
+const Move = styled.a`
+display: block;
+border-top: 1px solid #ececec;
+font-size: 14px;
+font-weight: 600;
+color: #36f;
+padding: 15px 20px;
+`;
+
+const ItemStyle = styled.img`
+width: 1060px;
+height: 300px;
 `;
 
 export default Item;
